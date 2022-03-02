c851074c96218302b7c1b93a0fbc24f5f8b8eb4a
Victor Hugo <victorh5900@gmail.com>
2022-03-01 20:20:45 -0400

    config<new-config>, importing libraries, configuring function realizaPedido

--- src/components/RealizaPedidos.vue
+++ src/components/RealizaPedidos.vue
@@ -1,20 +1,30 @@
 <script lang="ts" setup>
-import { reactive } from 'vue'
+import Swal from 'sweetalert2'
+// import axios from 'axios';
+import { reactive } from 'vue';
 
-const informacoesIniciais = {
+const info_pedido = reactive({
   tipoPedido: null,
   nomeCliente: '',
   opcaoCarne: '',
   tipoPagamento: null,
-}
+});
 
-const cliente = reactive({...informacoesIniciais})
-
-function realizaPedido() {
-  console.log(cliente)
+async function realizaPedido() {
+  // const response = await axios.post("/realizar-pedido", info_pedido)
+  Swal.fire(
+  'Bom trabalho!',
+  'Pedido adicionado a lista.',
+  'success'
+)
 }
 function cancelaPedido() {
-  Object.assign(cliente, informacoesIniciais)
+  Object.assign(info_pedido, {
+    tipoPedido: null,
+    nomeCliente: '',
+    opcaoCarne: '',
+    tipoPagamento: null,
+  })
 }
 </script>
 
@@ -22,16 +32,16 @@ function cancelaPedido() {
   <div id="realiza-pedidos">
     <div class="informacoes-pedidos">
       <p>Qual vai ser o tipo de pedido?</p>
-      <input type="radio" value="Marmitex" v-model="cliente.tipoPedido">
+      <input type="radio" value="Marmitex" v-model="info_pedido.tipoPedido">
       <label for="marmitex">Marmitex</label>
-      <input type="radio" value="Marmita" v-model="cliente.tipoPedido">
+      <input type="radio" value="Marmita" v-model="info_pedido.tipoPedido">
       <label for="marmita">Marmita</label>
       <p>Qual seu nome?</p>
-      <input type="text" v-model="cliente.nomeCliente">
+      <input type="text" v-model="info_pedido.nomeCliente">
       <p>Qual sua opção de carne?</p>
-      <input type="text" v-model="cliente.opcaoCarne">
+      <input type="text" v-model="info_pedido.opcaoCarne">
       <p>Qual tipo do pagamento?</p>
-      <select v-model="cliente.tipoPagamento">
+      <select v-model="info_pedido.tipoPagamento">
         <option>Dinheiro</option>
         <option>PIX</option>
         <option>Cartão</option>
