699b07f68276cbd14b696e8dc3c27c2ffc451d6a
Victor Hugo <victorh5900@gmail.com>
2022-03-03 00:29:35 -0400

    add<component>, adding component actions order

---
+++ src/components/ActionsOrder.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const emits = defineEmits(['make_order', 'cancel_order'])
+</script>
+
+<template>
+  <div class="acoes-pedido">
+    <button @click="emits('make_order')">Realizar Pedido</button>
+    <button @click="emits('cancel_order')">Cancelar Pedido</button>
+  </div>
+</template>
